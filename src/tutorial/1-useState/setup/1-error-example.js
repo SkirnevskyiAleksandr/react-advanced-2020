6003e0af346a72ad39c75a550bda27130acc3fea
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-11-15 22:51:04 +0200

    feat:useState-with-array

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -2,20 +2,69 @@ import React, { useState } from 'react';
 
 const ErrorExample = () => {
   const [text, setText] = useState("hello world")
-  const handleClick = () => {
+
+  const ChangeTitle = () => {
     if (text == "hello world") {
-      setText('Hello Sanya')
+      setText("Hi Sanya")
     } else {
       setText("hello world")
     }
+
   }
 
+
   return (
-    <React.Fragment>
+    <section>
       <h1>{text}</h1>
-      <btn className="btn" onClick={handleClick}>change button</btn>
-    </React.Fragment>
+      <button className="btn" onClick={ChangeTitle}>
+        change button
+      </button>
+    </section>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ErrorExample = () => {
+//   const [text, setText] = useState("hello world")
+//   const handleClick = () => {
+//     if (text == "hello world") {
+//       setText('Hello Sanya')
+//     } else {
+//       setText("hello world")
+//     }
+//   }
+
+//   return (
+//     <React.Fragment>
+//       <h1>{text}</h1>
+//       <btn className="btn" onClick={handleClick}>change button</btn>
+//     </React.Fragment>
+//   )
+// }
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,30 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data)
+
+  const clear = (id) => {
+    const newPeople = people.filter((name) => { return (name.id !== id) })
+    setPeople(newPeople)
+  }
+
+  return (
+    <>
+      {
+
+        people.map((persone) => {
+          const { id, name } = persone
+          return (
+            <div key={id} className="item">
+              <h4>{name}</h4>
+              <button className="btn" onClick={() => { clear(id) }}> clear</button>
+            </div>
+          )
+        })
+      }
+      <button className="btn" onClick={() => { setPeople([]) }}>delete button</button>
+    </>
+  )
 };
 
 export default UseStateArray;
